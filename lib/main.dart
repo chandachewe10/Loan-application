0b2f07c2befca3b97a9c8b3ad7c84bc4f6525362
chanda chewe <chewec03@gmail.com>
2023-08-12 16:58:37 +0200

    Project Structure

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const LoanMobileApplication());
+}
+
+class LoanMobileApplication extends StatelessWidget {
+  const LoanMobileApplication({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+
+    );
+  }
+}
